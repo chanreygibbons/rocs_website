1a1941a4b948ddd5048e942ed49786f366b0294a
chanreygibbons <117939421+chanreygibbons@users.noreply.github.com>
2024-10-28 20:18:03 +0200

    Update main.js

--- js/main.js
+++ js/main.js
@@ -24,6 +24,14 @@ const mobileMenuToggle = () => {
     const nav = document.querySelector('nav ul');
     nav.classList.toggle('show');
 };
+document.addEventListener("DOMContentLoaded", function () {
+    const menuIcon = document.querySelector(".menu-icon");
+    const navLinks = document.querySelector(".nav-links");
+
+    menuIcon.addEventListener("click", function () {
+        navLinks.classList.toggle("expanded");
+    });
+});
 
 // Scroll header behavior
 let lastScroll = 0;
